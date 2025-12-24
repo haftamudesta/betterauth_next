4f765cea403199b978f6a845d1b92ed62721bfed
Haftamu Desta <haftamudesta>
2025-12-24 00:58:39 -0800

    Add proxy

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "./db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
     database: prismaAdapter(db, {
@@ -8,5 +9,6 @@ export const auth = betterAuth({
     }),
     emailAndPassword:{
         enabled:true,
-    }
+    },
+    plugins:[nextCookies()]
 });
