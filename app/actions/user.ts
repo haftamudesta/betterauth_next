27c94159e3bb83c6a6a52eeae218444e3b9f36db
Haftamu Desta <haftamudesta>
2025-12-25 22:45:05 -0800

    Add updateProfile server function

---
+++ app/actions/user.ts
@@ -0,0 +1,20 @@
+import { authSession } from "@/lib/auth_utils";
+import { db } from "@/lib/db";
+
+
+export async function updateProfile(){
+    const session=await authSession()
+    if(!session){
+        throw new Error("Unauthorized...")
+    }
+    const user=await db.user.findUnique({
+        where:{id:session.user.id},
+        select:{
+            name:true,
+            email:true,
+            image:true,
+            twoFactorEnabled:true
+        }
+    })
+    return user;
+}
